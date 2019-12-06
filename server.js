92a95d1879e74fcfbffaf87cd5ff5454766621f3
Sai Kiran <sm4536@drexel.edu>
2019-12-06 00:42:56 -0500

    Cors Issue Resolved

--- server.js
+++ server.js
@@ -7,13 +7,33 @@ var express = require('express'),
   Products = require('./api/models/ProductsModel')
   bodyParser = require('body-parser');
 
-// mongoose instance connection url connection
+
+
+  // const MongoClient = require('mongodb').MongoClient;
+  // const uri = "mongodb+srv://sm4536:Phl@6789@cluster0-zyo5s.mongodb.net/Ecommerce?retryWrites=true&w=majority";
+  // const client = new MongoClient(uri, { useNewUrlParser: true });
+  // client.connect(err => {
+  //   const collection = client.db("test").collection("devices");
+  //   // perform actions on the collection object
+  //   client.close();
+  // });
+
+
+//mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/Ecommerce", { useNewUrlParser: true });
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 var routes = require('./api/routes/ordersRoutes')//importing route
